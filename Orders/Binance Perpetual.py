527a03c3f70a275479c0366a611cff0c74b71bcf
IgorWounds <74266147+IgorWounds@users.noreply.github.com>
2021-05-22 23:14:39 +0200

    Add files via upload

---
+++ Orders/Binance Perpetual.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from binance.client import Client
+from binance.enums import *
+
+api_key = '5LzrB7qgTo8JdE2FLyQaeETUzqe9VPnvuhyNZR8cnWiWaHKnEd63TtszjiittRel'
+api_secret = 'OAmvCjkfmznyrK176n7uN3vZYgLQ3rmHUnrmbkmRx9mprdFQNz5BrnV5UQtJUczJ'
+
+client = Client(api_key=api_key, api_secret=api_secret)
+
+order = client.futures_create_order(
+                        symbol='ETHUSDT',
+                        side=SIDE_BUY,
+                        type=ORDER_TYPE_LIMIT,
+                        quantity=0.006,
+                        timeInForce=TIME_IN_FORCE_GTC,
+                        price='2230'
+                        )
+print(order)
+
